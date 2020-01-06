4b5845bc94012506237ea1bfa0eab879e5bb5bfa
root <bandy773@gmail.com>
2020-01-06 18:42:05 +0900

    change keymap

--- ergodash/mini/tbando/keymap.c
+++ ergodash/mini/tbando/keymap.c
@@ -11,7 +11,8 @@ enum custom_keycodes {
   SYNBOL,
   FN_MOVE,
   SYMBOL,
-  SETTING
+  SETTING,
+  KC_BSES    // Back slash with non modifier, Escape with LCTL
 };
 
 // Layer related keycodes
@@ -25,38 +26,38 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
  //+--------+--------+--------+--------+--------+--------+--------+                 +--------+--------+--------+--------+--------+--------+--------+
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                   KC_EQL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_SPC,                    XXXXXXX, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSES, \
  //|--------+--------+--------+--------+--------+--------+--------|                 |--------+--------+--------+--------+--------+--------+--------|
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                   KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_ENT,                    KC_B   , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
  //|--------+--------+--------+--------+--------+--------+--------|                 |--------+--------+--------+--------+--------+--------+--------|
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_SPC,                    KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
- //|--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
-    KC_LCTL, KC_LGUI, KC_LALT, EISU,             SYMBOL,   KC_SPC,  KC_DEL,  KC_BSPC, KC_ENT , FN_MOVE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSPC,                   SYMBOL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+ //|--------+--------+--------+--------+--------+--------|        |--------+--------|        |--------+--------+--------+--------+--------+--------|
+    KC_RCTL, KC_LGUI, KC_LALT,          SYMBOL,  FN_MOVE, KC_BSPC, XXXXXXX, XXXXXXX, SYMBOL,  KC_SPC,  FN_MOVE,          KC_RALT, KC_RGUI, KC_RCTL  \
  //+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+
   ),
 
   [_SYMBOL] = LAYOUT(
- //+--------+--------+--------+--------+---------+--------+--------+                 +--------+--------+--------+--------+--------+--------+--------+
-    KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC, KC_UNDS,                   KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
- //|--------+--------+--------+--------+---------+--------+--------|                 |--------+--------+--------+--------+--------+--------+--------|
-    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,     KC_5,    KC_LCBR,                   KC_RCBR, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_COLN, KC_DQT , \
- //|--------+--------+--------+--------+---------+--------+--------|                 |--------+--------+--------+--------+--------+--------+--------|
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_SPC ,                   KC_ENT , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
- //|--------+--------+--------+--------+---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
-    KC_LCTL, KC_LGUI, KC_LALT, EISU,              SYMBOL,   KC_SPC , KC_DEL,  KC_BSPC, KC_ENT , FN_MOVE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
- //|--------+--------+--------+--------+---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ //+--------+--------+--------+--------+--------+--------+--------+                 +--------+--------+--------+--------+--------+--------+--------+
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    XXXXXXX,                   XXXXXXX, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, \
+ //|--------+--------+--------+--------+--------+--------+--------|                 |--------+--------+--------+--------+--------+--------+--------|
+    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, XXXXXXX,                   XXXXXXX, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, \
+ //|--------+--------+--------+--------+--------+--------+--------|                 |--------+--------+--------+--------+--------+--------+--------|
+    _______, KC_GRV,  KC_TILD, KC_EQL,  KC_PLUS, XXXXXXX, _______,                   _______, XXXXXXX, KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, XXXXXXX, \
+ //|--------+--------+--------+--------+--------+--------|        |--------+--------|        |--------+--------+--------+--------+--------+--------|
+    _______, _______, _______,          _______, _______, _______, XXXXXXX, XXXXXXX, _______, _______, _______,          _______, _______, _______  \
+ //|--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
   ),
 
   [_FN_MOVE] = LAYOUT(
- //+--------+--------+--------+--------+---------+--------+--------+                 +--------+--------+--------+--------+--------+--------+--------+
-    KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,   KC_PERC, KC_UNDS,                   KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
- //|--------+--------+--------+--------+---------+--------+--------|                 |--------+--------+--------+--------+--------+--------+--------|
-    KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_LCBR,                   KC_RCBR, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_COLN, KC_DQT , \
- //|--------+--------+--------+--------+---------+--------+--------|                 |--------+--------+--------+--------+--------+--------+--------|
-    KC_LSFT, KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_SPC ,                   KC_ENT,  KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
- //|--------+--------+--------+--------+---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
-    KC_LCTL, KC_F11 , KC_F12,  EISU,              SYMBOL,   KC_SPC , KC_DEL,  KC_BSPC, KC_ENT,  FN_MOVE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
- //|--------+--------+--------+--------+---------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+ //+--------+--------+--------+--------+--------+--------+--------+                 +--------+--------+--------+--------+--------+--------+--------+
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX, \
+ //|--------+--------+--------+--------+--------+--------+--------|                 |--------+--------+--------+--------+--------+--------+--------|
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, KC_F12,  \
+ //|--------+--------+--------+--------+--------+--------+--------|                 |--------+--------+--------+--------+--------+--------+--------|
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_DEL,                    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
+ //|--------+--------+--------+--------+--------+--------|        |--------+--------|        |--------+--------+--------+--------+--------+--------|
+    _______, _______, _______,          _______, _______, KC_DEL,  XXXXXXX, XXXXXXX, _______, KC_ENT,  _______,          _______, _______, _______  \
+ //|--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
     ),
 
   [_SETTING] = LAYOUT(
@@ -72,6 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+static bool lctrl_pressed = false;
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case SYMBOL:
@@ -94,14 +96,39 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case SETTING:
+
+    // back slash and escape key
+    case KC_BSES:
       if (record->event.pressed) {
-        layer_on(_SETTING);
+        if (lctrl_pressed) {
+          unregister_code(KC_LCTRL);
+          register_code(KC_ESC);
+        } else {
+          register_code(KC_BSLS);
+        }
       } else {
-        layer_off(_SETTING);
+        unregister_code(KC_ESC);
+        unregister_code(KC_BSLS);
+        if (lctrl_pressed) {
+          register_code(KC_LCTRL);
+        }
       }
       return false;
       break;
+  }
+  if (record->event.pressed) {
+    switch (keycode) {
+    case KC_LCTRL:
+      lctrl_pressed = true;
+      return true;
+    }
+  }
+  else if (!record->event.pressed) {
+    switch (keycode) {
+    case KC_LCTRL:
+      lctrl_pressed = false;
+      return false;
+    }
   }
   return true;
 }
